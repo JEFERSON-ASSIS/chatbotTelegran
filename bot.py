3f254d2090ebbd9e51bebacf5f78a4900ddcde14
JEFERSON-ASSIS <61355242+JEFERSON-ASSIS@users.noreply.github.com>
2020-04-11 10:04:13 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,39 @@
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters, RegexHandler, ConversationHandler)
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
+
+STATE1 = 1
+STATE2 = 2
 
 def welcome(update, context):
     message = 'Olá, ' + update.message.from_user.first_name + '!'
     print(message)
     context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+
+def Atendimento(update, context):
+    message = '''Qual tema você deseja saber mais hoje?\n
+        1- Resiliência \n
+        2- Autoconhecimento'''
+    update.message.reply_text(message, reply_markup=ReplyKeyboardMarkup([], one_time_keyboard=True)) 
+    return STATE1
+
+def inputAtendimento(update, context):
+    atendimento= update.message.text
+    print(atendimento)
+    if Atendimento == '1' or Atendimento == 'Resiliência' or Atendimento == 'resiliencia':
+        message = "https://www.youtube.com/watch?v=9PWedS78wfc&t=3s"
+        context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+        return STATE2
+    elif Atendimento == '2' or Atendimento == 'Autoconhecimento':
+        message = "https://www.youtube.com/watch?v=RZhXIpXlkKU"
+        context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+        return STATE2
+
+def inputAssunto(update, context):
+    message = "Muito obrigada pelo em breve novidades!!"
+    context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+
+def cancel(update, context):
+    return ConversationHandler.END   
 
 def main():
     token = '1182820862:AAHGMGWZI6LJPloox6sFKpZmPpebRGdnwbQ'
@@ -11,10 +41,23 @@ def main():
 
     updater.dispatcher.add_handler(CommandHandler('start', welcome))
 
+    conversation_handler = ConversationHandler(
+        entry_points=[CommandHandler('Atendimento', Atendimento)],
+        states={
+            STATE1: [MessageHandler(Filters.text, inputAtendimento)],
+            STATE2: [MessageHandler(Filters.text, inputAssunto)]
+        },
+
+        fallbacks=[CommandHandler('cancel', cancel)])
+
+
+    updater.dispatcher.add_handler(conversation_handler)
+   
     updater.start_polling()
     print(str(updater))
     updater.idle()
 
 if __name__ == "__main__":
     main()    
+      
     
